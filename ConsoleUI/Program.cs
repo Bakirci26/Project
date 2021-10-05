95191d1a6c92f0c3c4bae72cbe28a143f401b91f
MUSTAFA BAKIRCI <2013010217086@ogrenci.karabuk.edu.tr>
2021-10-05 16:16:14 +0300

    5.10.21

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,13 +5,13 @@ using System;
 
 namespace ConsoleUI
 {
-    class Program
+ class Program
     {
         static void Main(string[] args)
         {
 
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll()) 
+           foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
