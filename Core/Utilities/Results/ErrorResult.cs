232369fb94011d0e49273aa22cf6ae0ed41af841
MUSTAFA BAKIRCI <2013010217086@ogrenci.karabuk.edu.tr>
2021-10-08 15:15:10 +0300

    8.10.21 2.kısım 10

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorResult:Result
+    {
+        public ErrorResult(string messege) : base(false,messege)
+        {
+
+
+        }
+
+    public ErrorResult(): base(false)
+        {
+
+
+
+        }
+    }
+} 
